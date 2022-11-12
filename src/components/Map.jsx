44f0f5027b2c0161059edae343e8cf0367977fab
Oscar Mui <oscarmui1127@gmail.com>
2022-11-12 13:30:17 +0000

    map works

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,64 @@
 import React,{useEffect} from 'react';
+import GoogleMapReact from 'google-map-react';
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
-const GOOGLE_API_KEY = import.meta.env["REACT_APP_GOOGLE_API_KEY"];
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 const render = (status) => {
   return <h1>{status}</h1>;
 };
+
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+// function Map(){
+//   const defaultProps = {
+//     center: {
+//       lat: 10.99835602,
+//       lng: 77.01502627
+//     },
+//     zoom: 11
+//   };
+
+//   return (
+//     // Important! Always set the container height explicitly
+//     <div style={{ height: '100vh', width: '100%' }}>
+//       <GoogleMapReact
+//         bootstrapURLKeys={{ key: GOOGLE_API_KEY }}
+//         defaultCenter={defaultProps.center}
+//         defaultZoom={defaultProps.zoom}
+//       >
+//         <AnyReactComponent
+//           lat={59.955413}
+//           lng={30.337844}
+//           text="My Marker"
+//         />
+//       </GoogleMapReact>
+//     </div>
+//   );
+// }
 
 function Map(props) {
   const ref = React.useRef(null);
   const [map, setMap] = React.useState();
 
   useEffect(() => {
+    console.log(GOOGLE_API_KEY)
     if (ref.current && !map) {
-      setMap(new window.google.maps.Map(ref.current, {}));
+      setMap(new window.google.maps.Map(ref.current, {
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+        zoom: 4,
+      }));
+      console.log("if clause")
     }
+    console.log(map);
+    console.log(ref);
   }, [ref, map]);
 
   return (
-    <Wrapper apiKey={GOOGLE_API_KEY} render={render}>
-      <div ref={ref} style={{width:'100%',height:'400px',margin:'auto'}}/>
-    </Wrapper>
+    <div ref={ref} style={{width:'100%',height:'400px',margin:'auto', overflow: "visible"}}/>
   )
 } 
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Map from '../components/Map'
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+
+const render = (status) => {
+  return <h1>{status}</h1>;
+};
+
 
 function HomePage (props) {
   return (
       <>
         <div>Home page</div>
+        <Wrapper apiKey={GOOGLE_API_KEY} render={render}>
         <Map />
+      </Wrapper>
+        
       </>
   )
 }
